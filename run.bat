a64874f2d4ff87ff4c8ebd09f6733253cd717f79
pvzzombs <pvzzombs@gmail.com>
2021-05-28 06:43:39 +0000

    fix run.bat

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 start build/Release/Mandelbrot.exe
 ping 127.0.0.1 -n 10
-taskkill /im build/Release/Mandelbrot.exe /f
+taskkill /im Mandelbrot.exe /f
 
 start build/Release/Test.exe
 ping 127.0.0.1 -n 10
-taskkill /im build/Release/Test.exe /f
+taskkill /im Test.exe /f
 echo Done...
